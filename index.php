ddc1899b679ab8c9e1943fa391a7d5355e71bd44
ili <ilianagranados.05@gmail.com>
2025-05-12 22:08:18 -0600

    Guardando script js

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registro</title>
+    <link href="styles.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+</head>
+<body>
+    <section class="container">
+    <h1>Formulario de Registro</h1>
+
+    <form>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Nombre Completo </label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Correo</label>
+    <input type="password" class="form-control" id="exampleInputPassword1">
+  </div>
+<div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Fecha de Nacimiento</label>
+    <input type="password" class="form-control" id="exampleInputPassword1">
+  </div>
+<div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Contraseña</label>
+    <input type="password" class="form-control" id="exampleInputPassword1">
+  </div>
+
+  <div class="mb-3 form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" class="btn btn-primary" onclick="enviarForm()">Registrar</button>
+  </section>
+  <section>
+
+  </section>
+</form>
+
+<script src="js/script_registro.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+</body>
+</html>
